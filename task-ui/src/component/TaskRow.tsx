2a1aae52f484b18b1b66a1a8794c6ca6107547e8
SaraS <sara.stojilkova@bilans.dev>
2026-04-14 18:13:52 +0200

    feat: implemented complete button for tasks that aren't done

--- task-ui/src/component/TaskRow.tsx
+++ task-ui/src/component/TaskRow.tsx
@@ -1,16 +1,18 @@
 import type { Task } from "../types/api";
 import { StatusBadge } from "./StatusBadge";
+import { useCompleteTask } from "../hooks/useTasks";
 
 export function TaskRow({ task }: { task: Task }) {
+
+  const { mutate, isPending } = useCompleteTask(task.agency_id);
+  const isDone = task.status === "done";
+
   return (
     <>
       <td>{task.title}</td>
-
-      <td>
-        <StatusBadge status={task.status as any} />
-      </td>
-
+      <td><StatusBadge status={task.status} /></td>
       <td>{task.priority}</td>
+      <td>{!isDone && (<button onClick={() => mutate(task.id)} disabled={isPending}>Complete</button>)}</td>
     </>
   );
 }
